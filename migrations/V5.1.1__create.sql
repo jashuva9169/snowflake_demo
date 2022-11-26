6e55a355a22f5b541f4034100adadf508bef0e98
unknown <jashuva.u.mokka@kipi.bi>
2022-11-26 19:32:44 +0530

    d

---
+++ migrations/V5.1.1__create.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE public.test (
+    id character varying(12),
+    name character varying(23)
+);
+
+
+CREATE TABLE public.roles (
+    id character varying(12),
+    name character varying(23)
+);
